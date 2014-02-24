d3f6175e967d0d60e3528b0664a118a67b4645d5
francisco-navarro <packarbell@gmail.com>
2014-02-24 19:25:42 +0100

    Copia base app android

---
+++ app-caso-android/src/app/prueba/caso_android/android/LoginActivity.java
@@ -0,0 +1,12 @@
+package app.prueba.caso_android.android;
+
+import android.app.Activity;
+import app.prueba.caso_android.R;
+import app.prueba.caso_android.R.id;
+import app.prueba.caso_android.R.layout;
+
+
+
+public class LoginActivity extends Activity {
+	
+}
