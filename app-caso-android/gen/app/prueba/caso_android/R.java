758fd67639b2bcf9945b2b12145fadae1a80fab4
francisco-navarro <packarbell@gmail.com>
2014-02-24 22:33:49 +0100

    Estructura de pantallas android

--- app-caso-android/gen/app/prueba/caso_android/R.java
+++ app-caso-android/gen/app/prueba/caso_android/R.java
@@ -19,23 +19,48 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int buttonBarStyle=0x7f010000;
     }
     public static final class color {
+        public static final int amarillo_header=0x7f040007;
         public static final int black_overlay=0x7f040000;
+        public static final int blanco=0x7f040004;
+        public static final int fondo_item=0x7f040002;
+        public static final int gris_claro=0x7f040001;
+        public static final int gris_oscuro=0x7f040003;
+        public static final int negro=0x7f040005;
+        public static final int otronegro=0x7f040006;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int book=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int libro=0x7f020002;
+        public static final int list_item=0x7f020003;
     }
     public static final class id {
-        public static final int dummy_button=0x7f070002;
-        public static final int fullscreen_content=0x7f070000;
-        public static final int fullscreen_content_controls=0x7f070001;
+        public static final int cancel_login=0x7f080004;
+        public static final int imageView1=0x7f080001;
+        public static final int listViewBooks=0x7f080000;
+        public static final int login=0x7f080005;
+        public static final int login_pass=0x7f080003;
+        public static final int login_usuario=0x7f080002;
+        public static final int menu_settings1=0x7f080008;
+        public static final int menu_settings2=0x7f080009;
+        public static final int textViewFecha=0x7f080007;
+        public static final int textViewNombre=0x7f080006;
     }
     public static final class layout {
-        public static final int activity_login=0x7f030000;
+        public static final int activity_booklist=0x7f030000;
+        public static final int activity_login=0x7f030001;
+        public static final int book_item=0x7f030002;
+        public static final int list_view_header=0x7f030003;
+    }
+    public static final class menu {
+        public static final int list_book=0x7f070000;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
-        public static final int dummy_button=0x7f050001;
-        public static final int dummy_content=0x7f050002;
+        public static final int menu_order_date=0x7f050001;
+        public static final int menu_order_name=0x7f050002;
+        public static final int title_date=0x7f050004;
+        public static final int title_name=0x7f050003;
     }
     public static final class style {
         /** 
--- app-caso-android/src/app/prueba/caso_android/android/LoginActivity.java
+++ app-caso-android/src/app/prueba/caso_android/android/LoginActivity.java
@@ -1,12 +1,36 @@
 package app.prueba.caso_android.android;
 
+
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import app.prueba.caso_android.R;
-import app.prueba.caso_android.R.id;
-import app.prueba.caso_android.R.layout;
 
 
 
 public class LoginActivity extends Activity {
 	
+	private TextView textViewUser;
+	private TextView textViewPass;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		
+		textViewUser = (TextView) findViewById(R.id.login_usuario);
+		textViewPass = (TextView) findViewById(R.id.login_pass);
+	}
+	
+	public void doEntrar(View view) {
+		Intent mIntent = new Intent(getApplicationContext(),
+				ListBookActivity.class);
+		startActivity(mIntent);
+	}
+	
+	public void doCancelar(View view) {
+		
+	}
 }
